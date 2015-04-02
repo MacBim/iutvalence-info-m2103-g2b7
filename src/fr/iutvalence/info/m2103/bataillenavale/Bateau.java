9d5db0a054071ed3e9f771397661c3d9a719c2e4
MacBim <jean-baptiste.mercier@iut-valence.fr>
2015-04-02 09:14:45 +0200

    Création de la classe Bateau

---
+++ src/fr/iutvalence/info/m2103/bataillenavale/Bateau.java
@@ -0,0 +1,17 @@
+package fr.iutvalence.info.m2103.bataillenavale;
+/**
+ * Les bateaux de la partie
+ * @author Medy et Jean-Baptiste
+ *
+ */
+public abstract class Bateau
+{
+	private final int nbCaseOccupee;
+	private final String nom;
+	
+	
+	public Bateau()
+	{
+		
+	}
+}
