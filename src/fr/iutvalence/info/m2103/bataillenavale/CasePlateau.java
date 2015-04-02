264b2c081e42d07c46e381bb683b969e48a77da1
MacBim <jean-baptiste.mercier@iut-valence.fr>
2015-04-02 09:15:08 +0200

    Création de la classe CasePlateau

---
+++ src/fr/iutvalence/info/m2103/bataillenavale/CasePlateau.java
@@ -0,0 +1,13 @@
+package fr.iutvalence.info.m2103.bataillenavale;
+/**
+ * La classe CasePlateau
+ * @author Medy et Jean-Baptiste
+ *
+ */
+public class CasePlateau
+{
+	private int positionLigne;
+	private int positionColonne;
+	private boolean estOccupee;
+	
+}
