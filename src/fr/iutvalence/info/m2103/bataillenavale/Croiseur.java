ea98230a301c0ce8801e55e3d5a0067994797d33
MacBim <jean-baptiste.mercier@iut-valence.fr>
2015-04-02 09:27:57 +0200

    Création de la sous classe Croiseur

---
+++ src/fr/iutvalence/info/m2103/bataillenavale/Croiseur.java
@@ -0,0 +1,13 @@
+package fr.iutvalence.info.m2103.bataillenavale;
+
+public class Croiseur extends Bateau
+{
+	public final static int NB_CASE_OCCUPEE_CROISEUR=4;
+	
+	public Croiseur()
+	{
+		this.nbCaseOccupee = NB_CASE_OCCUPEE_CROISEUR;
+		this.nom="Croiseur";
+	}
+
+}
