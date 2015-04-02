b38f0103e37fbbd4c35c56cbf8a83498f216c1e0
MacBim <jean-baptiste.mercier@iut-valence.fr>
2015-04-02 09:29:45 +0200

    Création de la sous classe ContreTorpilleur

---
+++ src/fr/iutvalence/info/m2103/bataillenavale/ContreTorpilleur.java
@@ -0,0 +1,13 @@
+package fr.iutvalence.info.m2103.bataillenavale;
+
+public class ContreTorpilleur extends Bateau
+{
+
+	public final static int NB_CASE_OCCUPEE_CONTRE_TORPILLEUR=3;
+	
+	public ContreTorpilleur()
+	{
+		this.nbCaseOccupee = NB_CASE_OCCUPEE_CONTRE_TORPILLEUR;
+		this.nom="Contre Torpilleur";
+	}
+}
