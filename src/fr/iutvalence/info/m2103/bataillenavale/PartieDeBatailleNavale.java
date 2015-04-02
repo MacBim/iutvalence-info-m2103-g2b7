15a44813f06d57e2ef5a2cfeaa5ceb0189b5c8ee
MacBim <jean-baptiste.mercier@iut-valence.fr>
2015-04-02 09:16:53 +0200

    Mise à jour de la classe PartieDeBatailleNavale
    -création de la méthode jouer()
    -création d'une partie

--- src/fr/iutvalence/info/m2103/bataillenavale/PartieDeBatailleNavale.java
+++ src/fr/iutvalence/info/m2103/bataillenavale/PartieDeBatailleNavale.java
@@ -1,16 +1,26 @@
 package fr.iutvalence.info.m2103.bataillenavale;
 
 /**
- * 
+ * Une partie de Bataille Navale
  * @author Medy et Jean-Baptiste
  *
  */
 public class PartieDeBatailleNavale
 {
+	private final Joueur joueur1;
+	private final Joueur joueur2;
+		
+	
 	/** 
-	 * Constructeur de partie
+	 * Constructeur de la partie
 	 */
 	public PartieDeBatailleNavale()
+	{
+		this.joueur1 = new Joueur();
+		this.joueur2 = new Joueur();
+	}
+
+	public void jouer()
 	{
 		
 	}
