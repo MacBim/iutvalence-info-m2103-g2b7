5c521dd00c22fdc55ee5d3f3c6cb185833055780
MacBim <jean-baptiste.mercier@iut-valence.fr>
2015-04-02 08:16:37 +0200

    Déplacement des classes dans un paquetage
    - les classes sont maintenant définies dans le paquetage
    fr.iutvalence.info.m2103.bataillenavale

--- src/fr/iutvalence/info/m2103/bataillenavale/Partie.java
+++ src/fr/iutvalence/info/m2103/bataillenavale/Partie.java
@@ -1,14 +1,17 @@
+package fr.iutvalence.info.m2103.bataillenavale;
+
 /**
  * 
  * @author Medy et Jean-Baptiste
  *
  */
-public class Partie 
+public class Partie
 {
 	/** 
 	 * Constructeur de partie
 	 */
 	public Partie()
 	{
+		
 	}
 }
