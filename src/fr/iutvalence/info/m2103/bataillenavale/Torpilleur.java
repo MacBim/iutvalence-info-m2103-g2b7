8e5a03770863483633449a020e800dbca8fe94e0
MacBim <jean-baptiste.mercier@iut-valence.fr>
2015-04-02 09:32:20 +0200

    Création de la sous classe Torpilleur

---
+++ src/fr/iutvalence/info/m2103/bataillenavale/Torpilleur.java
@@ -0,0 +1,13 @@
+package fr.iutvalence.info.m2103.bataillenavale;
+
+public class Torpilleur extends Bateau
+{
+
+	public final static int NB_CASE_OCCUPEE_TORPILLEUR=2;
+	
+	public Torpilleur()
+	{
+		this.nbCaseOccupee = NB_CASE_OCCUPEE_TORPILLEUR;
+		this.nom="Torpilleur";
+	}
+}
